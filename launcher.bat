41f61ad2c4f6101705ae782d39787ef0b8c85884
Rais Iqbal <bahshat@users.noreply.github.com>
2025-04-17 16:44:14 +0530

    Update launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,8 +1,40 @@
 @echo off
 SETLOCAL
 
-:: Launch Ochoa silently (using PowerShell)
-powershell -command "Start-Process -FilePath '%APPDATA%\Microsoft\Windows\Start Menu\Programs\Ochoa.lnk'"
+:: 1. Launch Ochoa App (React Native Windows App)
+:: Try multiple common Start Menu locations
+if exist "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Ochoa.lnk" (
+    start "" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Ochoa.lnk"
+) else if exist "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Ochoa.lnk" (
+    start "" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Ochoa.lnk"
+) else (
+    echo Could not find Ochoa in Start Menu.
+    echo Manually locate Ochoa.exe and update this script.
+    pause
+    exit /b 1
+)
 
-:: Launch backend silently
-powershell -command "Start-Process -FilePath 'C:\path\to\run.exe'"
+:: 2. Launch Backend (run.exe)
+:: Check common installation paths
+set BACKEND_PATHS=(
+    "C:\Program Files\Ochoa\run.exe"
+    "C:\Program Files (x86)\Ochoa\run.exe"
+    "%LOCALAPPDATA%\Programs\Ochoa\run.exe"
+    "%~dp0run.exe"  %== Same folder as batch file ==%
+)
+
+for %%i in %BACKEND_PATHS% do (
+    if exist "%%i" (
+        start "" "%%i"
+        goto BACKEND_LAUNCHED
+    )
+)
+
+echo Could not find run.exe. Tried:
+for %%i in %BACKEND_PATHS% do echo   %%i
+pause
+exit /b 1
+
+:BACKEND_LAUNCHED
+echo Both Ochoa and backend launched successfully!
+timeout /t 3 >nul  %== Auto-close after 3 seconds ==%
