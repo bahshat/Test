7448ca38135b73fe4640d352781ca3ac7e4f2c45
Rais Iqbal <bahshat@users.noreply.github.com>
2025-05-22 14:50:28 +0530

    Update temp.tsx

--- temp.tsx
+++ temp.tsx
@@ -1,8 +1,2 @@
 $cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject "CN=OchoaTestCert" -CertStoreLocation "Cert:\LocalMachine\My"
-Export-Certificate -Cert $cert -FilePath "C:\OchoaTestCert.cer"
-
-Import-Certificate -FilePath "C:\OchoaTestCert.cer" -CertStoreLocation "Cert:\LocalMachine\TrustedPeople"
-
-signtool sign /fd SHA256 /a /f "C:\OchoaTestCert.cer" "path\to\your\App.msix"
-
-Add-AppxPackage "path\to\your\App.msix"
+Export-Certificate -Cert $cert -FilePath "$env:USERPROFILE\Desktop\OchoaTestCert.cer"
