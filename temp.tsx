536f2a53894727b0740f8d50d48267eafcca027b
Rais Iqbal <bahshat@users.noreply.github.com>
2025-05-22 15:59:49 +0530

    Update temp.tsx

--- temp.tsx
+++ temp.tsx
@@ -1,7 +1,11 @@
-& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool.exe" sign `
-  /fd SHA256 `
-  /f "$env:USERPROFILE\Desktop\OchoaTestCert.pfx" `
-  /p 1234 `
-  /tr http://timestamp.digicert.com `
-  /td SHA256 `
-  "C:\path\to\your\Ochoa.msix"
+$cert = New-SelfSignedCertificate `
+  -Type CodeSigningCert `
+  -Subject "CN=OchoaCert" `
+  -CertStoreLocation "Cert:\CurrentUser\My"
+
+$pwd = ConvertTo-SecureString "1234" -AsPlainText -Force
+
+Export-PfxCertificate `
+  -Cert $cert `
+  -FilePath "$env:USERPROFILE\Desktop\OchoaCert.pfx" `
+  -Password $pwd
