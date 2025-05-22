f69dbeadef58004232bb4eb16917eeb256a7370a
Rais Iqbal <bahshat@users.noreply.github.com>
2025-05-22 15:09:34 +0530

    Update temp.tsx

--- temp.tsx
+++ temp.tsx
@@ -1,1 +1,5 @@
-signtool sign /fd SHA256 /a /f "%USERPROFILE%\Desktop\OchoaTestCert.cer" "C:\path\to\your\app.msix"
+& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool.exe" sign `
+  /fd SHA256 `
+  /a `
+  /f "$env:USERPROFILE\Desktop\OchoaTestCert.cer" `
+  "C:\path\to\your\app.msix"
