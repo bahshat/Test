9e124e4d51b89d4354e96a72ca44698fb7c355c8
Rais Iqbal <bahshat@users.noreply.github.com>
2025-05-27 20:07:56 +0530

    Update temp.tsx

--- temp.tsx
+++ temp.tsx
@@ -1,17 +1,4 @@
-// __tests__/api-call.test.js
-
-const fetch = require('node-fetch');
-
-describe('API Test', () => {
-  it('should return suites from Flask', async () => {
-    const res = await fetch('http://127.0.0.1:5000/suites');
-    expect(res.status).toBe(200);
-
-    const data = await res.json();
-    console.log('DATA:', data);
-
-    expect(Array.isArray(data)).toBe(true);
-    expect(data.length).toBeGreaterThan(0);
-    expect(data[0]).toHaveProperty('id');
-  });
-});
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+};
